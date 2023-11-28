a365c74cb2d8f8bedcc551e7918e46bcec745920
PhilVoel <philipp123.voeller@gmail.com>
2023-11-28 20:02:46 +0100

    Added support for loading other running sessions which have not been saved

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -355,37 +355,47 @@ cleanup_restored_pane_contents() {
 	fi
 }
 
-get_saved_sessions() {
+get_sessions() {
 	local all_files="$(ls $(resurrect_dir))"
 	for file in $all_files; do
 		if [[ "$file" =~ _last$ ]] && [[ "${file%%_last}" != "$CURRENT_SESSION" ]]; then
 			echo "${file%%_last}"
 		fi
 	done
+	local all_sessions="$(tmux list-sessions -F "#{session_name}")"
+	for session in $all_sessions; do
+		if [[ "$session" != "$CURRENT_SESSION" ]]; then
+			echo "$session"
+		fi
+	done
 }
 
 main() {
 	if supported_tmux_version_ok; then
-		local resurrect_file="$(get_saved_sessions|fzf)"
-		if [[ -n "$resurrect_file" ]]; then
-		ln -fs "$(resurrect_dir)/${resurrect_file}_last" "$(resurrect_dir)/last"
-			start_spinner "Restoring..." "Tmux restore complete!"
-			execute_hook "pre-restore-all"
-			restore_all_panes
-			handle_session_0
-			restore_window_properties >/dev/null 2>&1
-			execute_hook "pre-restore-pane-processes"
-			restore_all_pane_processes
-			# below functions restore exact cursor positions
-			restore_active_pane_for_each_window
-			restore_zoomed_windows
-			restore_grouped_sessions  # also restores active and alt windows for grouped sessions
-			restore_active_and_alternate_windows
-			restore_active_and_alternate_sessions
-			cleanup_restored_pane_contents
-			execute_hook "post-restore-all"
-			stop_spinner
-			display_message "Tmux restore complete!"
+		local session_name="$(get_sessions|uniq|fzf)"
+		if [[ -n "$session_name" ]]; then
+			if [[ -n $(tmux list-sessions -F "#{session_name}" | grep "$session_name") ]]; then
+				tmux switch-client -t "$session_name"
+			else 
+				ln -fs "$(resurrect_dir)/${session_name}_last" "$(resurrect_dir)/last"
+				start_spinner "Restoring..." "Tmux restore complete!"
+				execute_hook "pre-restore-all"
+				restore_all_panes
+				handle_session_0
+				restore_window_properties >/dev/null 2>&1
+				execute_hook "pre-restore-pane-processes"
+				restore_all_pane_processes
+				# below functions restore exact cursor positions
+				restore_active_pane_for_each_window
+				restore_zoomed_windows
+				restore_grouped_sessions  # also restores active and alt windows for grouped sessions
+				restore_active_and_alternate_windows
+				restore_active_and_alternate_sessions
+				cleanup_restored_pane_contents
+				execute_hook "post-restore-all"
+				stop_spinner
+				display_message "Tmux restore complete!"
+			fi
 		fi
 	fi
 }
